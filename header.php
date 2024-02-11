31617c4b7a08747ef7bf1e41366f9a24029ea546
brk-cn <brkcndev@gmail.com>
2024-02-11 23:55:13 +0300

    login and signup forms

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-..." crossorigin="anonymous">
+  <title>Bookstore</title>
+</head>
+
+<body>
+  <header class="p-3">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <i class="fa-solid fa-book-open-reader me-3"></i>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 text-dark">Link</a></li>
+          <li><a href="#" class="nav-link px-2 text-dark">Link</a></li>
+          <li><a href="#" class="nav-link px-2 text-dark">Link</a></li>
+        </ul>
+
+        <div class="text-end">
+          <a href="login.html" type="button" class="btn">Login</a>
+          <a href="sign-up.html" type="button" class="btn btn-primary mx-3">Sign-up</a>
+        </div>
+      </div>
+    </div>
+  </header>
+</body>
+
+</html>
